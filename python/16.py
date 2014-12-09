e9291c87deded1ae572ab5a5bd285e97e2cbdbc3
ben_cripps <ben@benjamincripps.com>
2014-12-08 20:25:40 -0600

    15, redid git ignore

---
+++ python/16.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: ben_cripps
+# @Date:   2014-12-08 20:22:24
+# @Last Modified by:   ben_cripps
+# @Last Modified time: 2014-12-08 20:22:26
+
+print(sum([int(x) for x in list(str(2**1000))]))
