448522f3649eac643b39106ca2ae775656cc231a
ben_cripps <ben@benjamincripps.com>
2014-12-26 20:17:11 -0600

    better solution for clojure 5

--- clojure/proj5/src/proj5/core.clj
+++ clojure/proj5/src/proj5/core.clj
@@ -4,9 +4,10 @@
 (defn divisible-by-1-20 [n] 
     (every? #(zero? (rem n %)) (range 1 21)))
 
-(defn all-numbers [n]
-    (cons n (lazy-seq (all-numbers (inc n)))))
-
 (defn -main [& args]
     "Euler Project 5"
-    (println (first (filter divisible-by-1-20 (all-numbers 20)))))
+    (->>  (nthrest (range) 1)
+          (filter #(zero? (rem % 20)))
+          (filter divisible-by-1-20)
+          first
+          println))
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Project Euler Solutions
 ## Languages:
 - JavaScript
-- Python (To do)
-- Clojure (? Maybe)
+- Python
+- Clojure
