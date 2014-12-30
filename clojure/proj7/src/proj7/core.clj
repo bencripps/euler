611a028fbe59417385a375bf8b3e00eeedcd2942
ben_cripps <ben@benjamincripps.com>
2014-12-30 16:04:05 -0600

    fixed clojure 7

--- clojure/proj7/src/proj7/core.clj
+++ clojure/proj7/src/proj7/core.clj
@@ -2,15 +2,17 @@
   (:gen-class))
 
 (defn prime? [n]
-   (let [divisors (range 2 (inc (int (Math/sqrt n))))
-         remainders (map #(rem n %) divisors)]
-   (not (some zero? remainders))))
+    (and
+        (not (#{0 1} n))
+        (let [divisors (range 2 (inc (int (Math/sqrt n))))
+              remainders (map #(rem n %) divisors)]
+        (not (some zero? remainders)))))
 
 (defn -main [& args]
     "Euler Project 7"
     (->>
-        (nthrest (range) 1)
+        (range)
         (filter prime?)
-        (take 10002)
+        (take 10001)
         last
         println))
