0b79cd7928a300c8ba1b1914ff97daa7848d103f
ben_cripps <ben@benjamincripps.com>
2015-01-02 12:03:24 -0600

    clojure 15

---
+++ clojure/proj15/src/proj15/core.clj
@@ -0,0 +1,16 @@
+(ns proj15.core
+  (:gen-class))
+
+(defn square [n]
+    (* n n))
+
+(defn factorial [n]
+    (apply * (range (bigint 1) (inc n))))
+
+(defn -main [& args]
+  "Euler Project 15"
+  (let [routes (factorial 20)]
+    (->> 
+        (* routes routes)
+        (/ (factorial (* 2 20)))
+        println)))
