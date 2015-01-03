5e1b12ee5e98c8f55134b8e7f247a74a315f1dc8
ben_cripps <ben@benjamincripps.com>
2015-01-03 14:11:32 -0600

    complying with pythonic customs

--- python/5.py
+++ python/5.py
@@ -3,18 +3,18 @@
 # @Author: ben_cripps
 # @Date:   2015-01-03 10:33:19
 # @Last Modified by:   ben_cripps
-# @Last Modified time: 2015-01-03 14:04:04
+# @Last Modified time: 2015-01-03 14:10:27
 import math
 
 def isPrime(n):
     return n != 1 and len([x for x in range(2, n) if n % x == 0]) == 0
 
-def getExponent(n,x):
+def get_exponent(n,x):
     return math.floor(math.log(n,x))
 
-def smallesNumberDivisibleBy(n):
-    primeFactors = filter(isPrime, [x for x in range(2,n)])
-    numberOfPrimes = map(lambda x: math.pow(x, getExponent(n,x)), primeFactors)
-    return reduce(lambda x,y: x*y, numberOfPrimes)
+def smallest_number_divisible_by(n):
+    prime_factors = [x for x in range(2,n) if isPrime(x)]
+    number_of_primes = map(lambda x: math.pow(x, get_exponent(n,x)), prime_factors)
+    return reduce(lambda x,y: x*y, number_of_primes)
 
-print(smallesNumberDivisibleBy(20))
+print(smallest_number_divisible_by(20))
